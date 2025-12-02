17d8b1ef3d5b9d3eb55268d449bd430280c94b27
Izabella Marton <izabella.marton@siemens.com>
2025-12-02 17:06:15 +0100

    episode-03

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
+import { jsx } from "react/jsx-runtime";
 
-const heading1 = React.createElement("h1", {key: "5",}, "I'm an h1 tag");
-const heading2 = React.createElement("h2", {key: "4",}, "I'm an h2 tag");
-const child = React.createElement("div", { key: "3", id: "child" }, [heading1, heading2]);
-const child2 = React.createElement("div", { key: "2", id: "child2" }, [heading1, heading2]);
-const parent = React.createElement("div", { key: "1", id: "parent" }, [child, child2]);
+const Title = () => (
+  <h1 className="head" tabIndex="1">
+    Namaste React using JSX
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1>Namaste React Functional Component with arrow function</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); 
+
+root.render(<HeadingComponent />);
